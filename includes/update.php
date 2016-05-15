d3da8e0dcd2b039d6dbcd4b44af81db7e1a63030
pistell <jamespistell@gmail.com>
2016-05-15 17:59:51 -0400

    AJAX POST to MySQL DB config

--- includes/update.php
+++ includes/update.php
@@ -20,9 +20,10 @@ try {
   $total_likes = $_POST['total_likes'];
   $avg_likes = $_POST['avg_likes'];
   $engagement_ratio = $_POST['engagement_ratio'];
+  $picture = (string)$_POST['picture'];
 
   //Query shit (var from Form Shit (the DB column) compared to Ajax var)
-  $q = "INSERT INTO IG_Users(num_followers, user_id, username, total_comments, avg_comments, total_likes, avg_likes, engagement_ratio) VALUES(:num_followers, :user_id, :users_name, :total_comments, :avg_comments, :total_likes, :avg_likes, :engagement_ratio)";
+  $q = "INSERT INTO IG_Users(num_followers, user_id, username, total_comments, avg_comments, total_likes, avg_likes, engagement_ratio, picture) VALUES(:num_followers, :user_id, :users_name, :total_comments, :avg_comments, :total_likes, :avg_likes, :engagement_ratio, :picture)";
 
     $query = $dbc->prepare($q);
     $query->bindParam(':num_followers', $num_followers);
@@ -34,6 +35,7 @@ try {
     $query->bindParam(':total_likes', $total_likes);
     $query->bindParam(':avg_likes', $avg_likes);
     $query->bindParam(':engagement_ratio', $engagement_ratio);
+    $query->bindParam(':picture', $picture);
 
     $results = $query->execute();
 } catch (PDOException $e) {
