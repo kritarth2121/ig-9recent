a46eda281ca753ec149601103f726fb44827d756
Joseph Fusco <hello@josephfus.co>
2016-05-17 01:18:01 -0400

    Add everything again.

---
+++ database.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once('startup.php');
+require_once('config.php');
+
+try {
+  $db = new PDO("mysql:host=" . DB_HOST . ";
+                       dbname=" . DB_NAME,
+                       DB_USERNAME, //db username
+                       DB_PASSWORD); //DB password
+  //var_dump($db); //Dump the database info
+  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); //Shows errors if the PDO object returns errors such as cannot execute query
+                                                               //in PHP the 2 colons "::" represent a relationship between the class name on the left and the property or method on the right
+  //$db->exec("SET NAMES 'utf8'"); //set standard character set
+
+} catch (Exception $e) {
+  echo "Could not connect to DB"; //Display message if you cant connect
+  exit; //end connection
+}
+
+try{
+  $results = $db->query("SELECT * FROM IG_Users");
+} catch (PDOException $e){
+  echo 'ERROR: ' . $e->getMessage();
+  exit;
+}
+
+  //echo "<pre>";
+  //var_dump($results->fetchAll(PDO::FETCH_ASSOC));
+
+ ?>
+ <?php
+/*
+try{
+
+
+$servername = "host-10";
+$username = "root";
+$password = DB_PASSWORD;
+$dbname = "9recent";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+$conn->setAttribute(mysqli::ATTR_ERRMODE,mysqli::ERRMODE_EXCEPTION);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT * FROM 9recent";
+//$result = $conn->query($sql);
+$result = mysql_query($sql);
+while ($rows = mysql_fetch_array($result)):
+    $user_id = $rows['user_id'];
+    $username = $rows['username'];
+    $total_comments = $rows['total_comments'];
+    $total_likes = $rows['total_likes'];
+    $avg_likes = $rows['avg_likes'];
+    $engagement_ratio = $rows['engagement_ratio'];
+    $picture = $rows['picture'];
+    $time_stamp = $rows['time_stamp'];
+    $num_followers = $rows['num_followers'];
+
+echo "Username: {$row['username']}";
+endwhile;
+} catch (Exception $e){
+  echo "Didnt work";
+  exit;
+}
+
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["user_id"]. " - Name: " . $row["username"]. " " . $row["total_comments"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
+?>
+
+*/
---
+++ includes/update.php
@@ -0,0 +1,50 @@
+<?php
+
+try {
+  //connection shit
+  $host = "host-10";
+  $user = "killbot";
+  $pass = "kill";
+  $database = "9recent";
+  //PDO shit
+  $dbc = new PDO("mysql:host=" . $host . ";dbname=" . $database, $user, $pass);
+  $dbc->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+  //Form shit
+  $num_followers = $_POST['num_followers'];
+  $user_id = $_POST['user_id'];
+  //$username is the name of the column in the DB, users_name is the name of the AJAX var
+  $username = $_POST['users_name'];
+  $total_comments = $_POST['total_comments'];
+  $avg_comments = $_POST['avg_comments'];
+  $total_likes = $_POST['total_likes'];
+  $avg_likes = $_POST['avg_likes'];
+  $engagement_ratio = $_POST['engagement_ratio'];
+  $picture = $_POST['picture'];
+
+  //Query shit (var from Form Shit (the DB column) compared to Ajax var)
+  $q = "INSERT INTO IG_Users(num_followers, user_id, username, total_comments, avg_comments, total_likes, avg_likes, engagement_ratio, picture) VALUES(:num_followers, :user_id, :users_name, :total_comments, :avg_comments, :total_likes, :avg_likes, :engagement_ratio, :picture)";
+
+    $query = $dbc->prepare($q);
+    $query->bindParam(':num_followers', $num_followers);
+    $query->bindParam(':user_id', $user_id);
+    //Ajax var compared to var from Form Shit (the DB column)
+    $query->bindParam(':users_name', $username);
+    $query->bindParam(':total_comments', $total_comments);
+    $query->bindParam(':avg_comments', $avg_comments);
+    $query->bindParam(':total_likes', $total_likes);
+    $query->bindParam(':avg_likes', $avg_likes);
+    $query->bindParam(':engagement_ratio', $engagement_ratio);
+    $query->bindParam(':picture', $picture);
+
+    $results = $query->execute();
+} catch (PDOException $e) {
+  echo 'ERROR: ' . $e->getMessage();
+  exit;
+}
+
+
+
+
+
+?>
