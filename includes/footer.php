29e311f929fc06cd75a7d7c4c6eced533936127d
pistell <jamespistell@gmail.com>
2016-05-25 23:54:03 -0400

    added info to footer

--- includes/footer.php
+++ includes/footer.php
@@ -9,5 +9,12 @@ if ( ! defined( 'ABSPATH' ) ) exit ( "Direct access not permitted." );
 
 <script type="text/javascript" src="js/app.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" charset="utf-8"></script>
-</body>
+
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Project by <a href="https://github.com/pistell" target="_blank">James Pistell</a></p>
+        <p class="text-muted">With Assistance from <a href="https://github.com/josephfusco" target="_blank">Joseph Fusco</a></p>
+      </div>
+    </footer>
+  </body>
 </html>
