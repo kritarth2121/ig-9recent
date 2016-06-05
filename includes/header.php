89e2d00ae19681c9e252706c8a15fb81d90ecdf3
pistell <jamespistell@gmail.com>
2016-06-05 12:02:40 -0400

    added favicon

--- includes/header.php
+++ includes/header.php
@@ -12,8 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) exit ( "Direct access not permitted." );
      <meta name="robots" content="noarchive">
      <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
      <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link rel="icon" type="image/png" sizes="150x150" href="<?php echo get_data( 'profile_picture' ); ?>">
-     <link rel="apple-touch-icon" sizes="150x150" href="<?php echo get_data( 'profile_picture' ); ?>">
+     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
      <?php load_theme_styles(); ?>
      <script type="text/javascript" src="js/jquery.min.js"></script>
